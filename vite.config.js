1a7766adb1d6f80ffcf956e949d67ff4c1ff0bf2
varuncareergize <varun.124150@gmail.com>
2026-06-11 00:24:40 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite' // <-- 1. Import Tailwind
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/fleetops-pro/',
+  base: '/Fleetops-pro/',
   plugins: [
     react(),
     tailwindcss(), // <-- 2. Add Tailwind to your plugins array
